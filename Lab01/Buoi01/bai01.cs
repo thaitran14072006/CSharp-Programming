d3fd27fb9512d1fecb015381da0396cb08aa7994
jenny <phamquynhnhu07102006@gmail.com>
2026-09-15 22:10:09 +0700

    add bai1/buoi1

---
+++ Lab01/Buoi01/bai01.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class bai01
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("****************************");
+        Console.WriteLine("* Truong: Dai Hoc HUFLIT   *");
+        Console.WriteLine("* Khoa: CNTT               *");
+        Console.WriteLine("* Ho ten:                  *");
+        Console.WriteLine("****************************");
+    }
+}
